e750028f8406b9d24940f2e8ea74b1f96c932fe2
LuisPazzanese <luisfpp1@al.insper.edu.br>
2023-11-14 13:15:08 -0300

    feat: colisoes com as paredes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
 
-width , height = 800 , 600
-win = pygame.display.set_mode((width,height))
+WIDTH , HEIGHT = 800 , 600
+win = pygame.display.set_mode((WIDTH,HEIGHT)) #window
 pygame.display.set_caption('Brick Breaker')
 
 FPS = 60
-
-class Platform:
-    vel = 5
+platform_width = 100
+platform_height = 15
+ball_radius = 10
+class Platform: #plataforma
+    VEL = 5
 
     def __init__(self, x, y, width, height, color):
         self.x = x
@@ -17,17 +19,61 @@ class Platform:
         self.color = color
 
     def draw(self,win):
-        pygame.draw.rect(win,self.color(),(self.x,self.y,self.width,self.height))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+
+    def movement(self, direction=1):
+        self.x = self.x + self.VEL * direction
+
+class Ball: #bola
+    VEL = 5
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.x_vel = 2
+        self.y_vel = -self.VEL
 
 
-def draw(win): #empate
+    def movement(self): #movimento da bola
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def set_velocity(self,x_vel,y_vel): #vel da bola
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
+    def draw(self,win): #desenha a bolinha
+        pygame.draw.circle(win,self.color,(self.x,self.y),self.radius)
+
+
+
+
+
+
+
+def draw(win,platform,ball): #colorir
     win.fill('white')
+    platform.draw(win)
+    ball.draw(win)
     pygame.display.update()
 
-
+def ball_colission(ball):
+    if ball.x -ball_radius <= 0 or ball.x + ball_radius>= WIDTH:
+        ball.set_velocity(ball.x_vel * -1,ball.y_vel)
+    if ball.y + ball_radius>= HEIGHT or ball.y - ball_radius <= 0:
+        ball.set_velocity(ball.x_vel, ball.y_vel * -1)
+    
 
 def main():
     clock = pygame.time.Clock()
+
+    platform_x = WIDTH/2 - platform_width / 2
+    platform_y = HEIGHT - platform_height - 5
+    platform = Platform(platform_x, platform_y,platform_width ,platform_height, 'black')
+
+    ball = Ball(WIDTH/2, platform_y - ball_radius, ball_radius,'black')
 
     run = True
     while run:
@@ -37,7 +83,17 @@ def main():
                 run = False
                 break
 
-        draw(win)
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_LEFT] and platform.x - platform.VEL >= 0:
+                platform.movement(-1)
+            if keys[pygame.K_RIGHT] and platform.x + platform.width + platform.VEL <= WIDTH:
+                platform.movement(1)
+        
+        ball.movement()
+        ball_colission(ball)
+        draw(win, platform, ball)
+        
 
     pygame.quit()
     quit()
